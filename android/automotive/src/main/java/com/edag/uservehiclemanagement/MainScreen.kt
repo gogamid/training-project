f54de21924d54566b298316addc3b4d9367c7410
gogamid <gamidllimron@gmail.com>
2022-07-16 22:39:57 +0200

    feat(aaos): implement main screen with Roles and Vehicles rows

--- android/automotive/src/main/java/com/edag/uservehiclemanagement/MainScreen.kt
+++ android/automotive/src/main/java/com/edag/uservehiclemanagement/MainScreen.kt
@@ -1,14 +1,42 @@
 package com.edag.uservehiclemanagement
 
 import androidx.car.app.CarContext
+import androidx.car.app.CarToast
 import androidx.car.app.Screen
 import androidx.car.app.model.*
 
 class MainScreen(carContext: CarContext) : Screen(carContext) {
+
     override fun onGetTemplate(): Template {
-        val row = Row.Builder().setTitle("Hello World!").build()
-        return PaneTemplate.Builder(Pane.Builder().addRow(row).build())
-            .setHeaderAction(Action.APP_ICON)
+
+        val rolesRow = Row.Builder()
+            .setOnClickListener { navigateToRoles() }
+            .setTitle("Roles")
+            .addText("Get the roles of users")
+            .build()
+
+        val vehiclesRow = Row.Builder()
+            .setOnClickListener { navigateToVehicles() }
+            .setTitle("Vehicles")
+            .addText("Get the vehicles of the users")
+            .build()
+
+        val listBuilder = ItemList.Builder()
+            .addItem(rolesRow)
+            .addItem(vehiclesRow)
+
+        return ListTemplate.Builder()
+            .setSingleList(listBuilder.build())
+            .setTitle("Users")
             .build()
     }
+
+    private fun navigateToVehicles() {
+        CarToast.makeText(carContext, "Vehicles screen should show up", CarToast.LENGTH_LONG).show()
+    }
+
+    private fun navigateToRoles() {
+        CarToast.makeText(carContext, "Roles screen should show up", CarToast.LENGTH_LONG).show()
+    }
+
 }
